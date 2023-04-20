9a355546c96f401003ead07db0d6c9b8be16c1a6
Andrii Syzov <new.creator@icloud.com>
2023-04-20 13:38:42 +0300

    First Design My Project

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>My Project</title>
+</head>
+<body>
+    <div class="wrapper">
+
+        <header>
+            <img src="img/logo.png" alt="logotype">
+        </header>
+
+        <div class="container">
+            <nav class="nav">
+                <ul>
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">Новости</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Авторизация</a></li>|
+                    <li><a href="#">Регистрация</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="container">
+            <main>
+                <h1 class="main__title">Здесь должен быть красивый текст</h1>
+            </main>
+        </div>
+
+    </div>
+
+    <footer>
+        <p>Футер сайта</p>
+    </footer>
+</body>
+</html>
