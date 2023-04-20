8cbd734f0b63d644a7ff274e682faba107efa0ca
Andrii Syzov <new.creator@icloud.com>
2023-04-20 15:35:03 +0300

    Enumerating an associative array and outputting it to posts

--- news.php
+++ news.php
@@ -1,56 +1,23 @@
 <?php
 
 $title = 'Новости';
-
+include 'posts.php';
 include 'vendor/header.php';
 include 'vendor/navigation.php';
 include 'vendor/main.php';
-
 echo '<h1 class="news__title">Новости</h1>
-            <div class="news">
-                <div class="card">
-                    <img src="https://discuss.dev.twitch.tv/uploads/default/original/2X/4/40cfa8ca1f24ac29cfebcb1460b5cafb213b6105.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                    </div>
-                </div>
+            <div class="news">';
 
-                <div class="card">
-                    <img src="https://discuss.dev.twitch.tv/uploads/default/original/2X/4/40cfa8ca1f24ac29cfebcb1460b5cafb213b6105.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                    </div>
-                </div>
+foreach ($post as $value) {
 
-                <div class="card">
+        echo '<div class="card">
                     <img src="https://discuss.dev.twitch.tv/uploads/default/original/2X/4/40cfa8ca1f24ac29cfebcb1460b5cafb213b6105.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                        <p class="card-text">' . $value . '</p>
                     </div>
-                </div>
-            </div>
-            <div class="news">
-                <div class="card">
-                    <img src="https://discuss.dev.twitch.tv/uploads/default/original/2X/4/40cfa8ca1f24ac29cfebcb1460b5cafb213b6105.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img src="https://discuss.dev.twitch.tv/uploads/default/original/2X/4/40cfa8ca1f24ac29cfebcb1460b5cafb213b6105.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <img src="https://discuss.dev.twitch.tv/uploads/default/original/2X/4/40cfa8ca1f24ac29cfebcb1460b5cafb213b6105.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                    </div>
-                </div>
-            </div>';
+                </div>';
+}
+echo '</div>';
 
 include 'vendor/footer.php';
 ?>
---
+++ posts.php
@@ -0,0 +1,7 @@
+<?php
+
+$post = [
+    '1' => 'First Post',
+    '2' => 'Second Post',
+    '3' => 'Third Post',
+];
